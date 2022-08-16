b6f06cecbf70a5e81c8ec43377d0853f09a9870d
raghavareddy <raghavareddy2325@gmail.com>
2022-08-16 17:16:20 +0530

    code changed

--- arithmetic.js
+++ arithmetic.js
@@ -7,8 +7,28 @@ console.log('hello ' + 'everyone');
 
 console.log(2001 + ': A Space Odyssey');
 
-
+//addition
 var x=3;
 var y=5;
 var z=x+y;
 console.log(z);
+
+//subraction
+var x=5;
+var y=3;
+var z=x-y;
+console.log(z);
+
+//multiplication
+var x=5;
+var y=3;
+var z=x*y;
+console.log(z);
+
+//division
+var x=5;
+var y=3;
+var z=x/y;
+console.log(z);
+
+
