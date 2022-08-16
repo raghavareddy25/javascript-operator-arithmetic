c3115ad6dfa0189e75a150bf35d2a772e707b9cd
raghavareddy <raghavareddy2325@gmail.com>
2022-08-16 16:54:11 +0530

    code added

---
+++ arithmetic.js
@@ -0,0 +1,14 @@
+console.log(2 + 2);
+console.log(2 + true);
+
+
+console.log('hello ' + 'everyone');
+
+
+console.log(2001 + ': A Space Odyssey');
+
+
+var x=3;
+var y=5;
+var z=x+y;
+console.log(z);
